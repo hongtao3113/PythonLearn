b8bfc8257f93ce4816c3e3e40582fd9948fa2eae
wangkai <1357760345@qq.com>
2019-10-14 19:36:46 +0800

    添加代码注释，字符串替换

--- py-basic/ios/modfiyFileNames.py
+++ py-basic/ios/modfiyFileNames.py
@@ -1,21 +1,26 @@
 # 修改文件名称
 # coding=utf-8
-import os;
+# /bin/py3.6
+import os
 
 
 def rename():
-    path = "E:\\mydata\\commonFiles\\2019\\03\\16";
-    fileList = os.listdir(path);
-    print("文件列表：", fileList);
+    path = "F:\\idea_workspace\\scm-hegang\\scm-std-ap\\scm-std-admin\\src\\main\\webapp\\bank\\offLineFact\\apply\\js"
+    fileList = os.listdir(path)
+    print("文件列表：", fileList)
     for file in fileList:
-        oldFile = os.path.join(path, file);
+        oldFile = os.path.join(path, file)
         if os.path.isdir(oldFile):
-            continue;
-        filename = os.path.splitext(file)[0];
-        print("oldFile:", filename)
-        filetype = os.path.splitext(file)[1];
-        newFile = os.path.join(path, "test" + filetype);
-        os.rename(oldFile, newFile);
+            continue
+        filename = os.path.splitext(file)[0]
+        if 'msFact' in filename:
+            filename = filename.replace('msFact', 'offLineFact')
+            pass
+        print("oldFile:", filename)  # 文件名
+        filetype = os.path.splitext(file)[1]
+        print("filetype:", filetype)  # 文件类型 .tpl
+        newFile = os.path.join(path, filename + filetype)
+        os.rename(oldFile, newFile)
 
 
-rename();
+rename()
