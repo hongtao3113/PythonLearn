0528a9d6dccd4935ec830d686008a5967a3a2999
KAI丶丶丶 <1357760345@qq.com>
2020-12-22 17:23:08 +0800

    读取excel

---
+++ py-test/readExcel2.py
@@ -0,0 +1,45 @@
+import openpyxl
+
+filePath = 'C:\\Users\\wk\\Desktop\\龙票易信短信模版.xlsx'
+wb = openpyxl.load_workbook(filePath)
+# 获取所有的工作表名
+sheetNames = wb.sheetnames
+print("获取所有的工作表名:", sheetNames)
+smsSheet = wb[sheetNames[0]]
+print("获取指定的工作表:", smsSheet)
+maxRow = smsSheet.max_row
+print("最大行数:", maxRow)
+maxColumn = smsSheet.max_column
+print("最大列数:", maxColumn)
+print()
+
+# 循环工作簿的所有行
+for row in smsSheet.iter_rows():
+    bColumn = row[1]  # 编号所在的列
+    code = bColumn.value
+    if code != '编号':  # 排除第一行
+        gColumn = row[6]  # 发送内容所在的列
+        content = gColumn.value
+        # 打印
+        title = row[4].value
+        if content is not None:
+            code = bColumn.value.replace("-", "")
+            h = row[7].value
+            i = row[8].value
+            if h is None:
+                h = '系统'
+            if i is None:
+                i = ''
+            receiver = h + '-' + i
+
+            f = row[5].value
+            if f is None:
+                f = ''
+
+            # print(type(code),type(title),type(content))
+            if title is None:
+                title = ""
+            sendTime = title + "-" + f
+            result = code + '("' + sendTime + '",' + '"' + content + '","' + receiver + '"),'
+            # print(code, content)
+            print(result)
